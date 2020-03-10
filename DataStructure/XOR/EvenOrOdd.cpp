27d9e1cf158632bda1b60baff789bc4dfbbdc93f
nitish166 <knitish123@gmail.com>
2020-03-10 21:42:49 +0530

    Xor related Problem

---
+++ DataStructure/XOR/EvenOrOdd.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define mod 1000000007
+typedef long long int LL;
+ 
+int main()
+{
+ 
+   ios_base:: sync_with_stdio(false); cin.tie(0);
+   int n;
+   cin>>n;
+   if(n^1==n+1)
+   {
+   		cout<<"Even Number"<<endl;
+   }
+   else
+   {
+   		cout<<"Odd Number"<<endl;
+   }
+   
+        
+        
+ 
+   return 0;
+}
