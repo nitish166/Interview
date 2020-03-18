5283764e86cd60e0bffe5b97eab4c3d2f1dd49b3
nitish166 <knitish123@gmail.com>
2020-03-18 22:55:08 +0530

    Updated

--- DataStructure/Graph/Xor.cpp
+++ DataStructure/Graph/Xor.cpp
@@ -1,55 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
+ 
+#define int long long int
 #define endl "\n"
-#define mod 1000000007
-typedef long long int LL;
- 
-
- 
-int main()
+int32_t main()
 {
- 
-   ios_base:: sync_with_stdio(false); cin.tie(0);
-    
-   long long int t;
+   // freopen("1.in","r",stdin);
+   // freopen("1.out","w",stdout);
+   ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+   int t;
    cin>>t;
    while(t--)
    {
-         long long int n,q;
-         cin>>n>>q;
-         long long int seq[n];
-         long long int i;
-         for( i=0;i<n;i++)
+      int n,q;
+      cin>>n>>q;
+      int a[n];
+      for(int i=0;i<n;i++)
+         cin>>a[i];
+      int odd=0,even=0;
+      for(int i=0;i<n;i++)
+      {
+         int check=__builtin_popcount(a[i]);
+         if( check%2==0)
+            even++;
+         else
+            odd++;
+      }
+      while(q--)
+      {
+         int input;
+         cin>>input;
+         int odd1=odd,even1=even;
+         int nodd=0,neven=0;
+         int check1=__builtin_popcount(input);
+         if(check1%2!=0)
          {
-            cin>>seq[i];
-         }
-         long long int even=0, odd=0;
-         for( i=0;i<n;i++)
-         {
-             if(seq[i] &1 ==1)
-             {
-                odd++; 
-             }
-             else
-             {
-                 even++;
-             }
-         }
-         long long int qur[q];
-         for( i=0;i<q;i++)
-         {
-            cin>>qur[i];
-         }
-          
-          for( i=0;i<q;i++)
-          {
-              
-          }
-
-         
-   }
-        
-        
+            
+            odd1=even;
+            even1=odd;
  
+         }
+         cout<<even1<<" "<<odd1<<endl;
+      }
+ 
+   }
    return 0;
 }
