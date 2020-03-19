bf5e4af8755b390f4700e150cd7dd12feca47c7d
nitish166 <knitish123@gmail.com>
2020-03-19 18:25:48 +0530

    Duplicate element

---
+++ DataStructure/Arrays/secondGreaterElement.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+
+	if(n<2)
+	{
+		return -1;
+	}
+
+	int first=INT_MIN, second =INT_MAX;
+	for(int i=0;i<n;i++)
+	{
+		if(arr[i]>first)
+		{
+			second=first;
+			first=arr[i];
+		}
+		else if(arr[i]>second && arr[i] !=first)
+		{
+			second = arr[i];
+		}
+	}
+
+	if(second==INT_MIN)
+	{
+		return -1;
+	}
+	else
+	{
+		cout<<second<<endl;
+	}
+return 0;
+}
