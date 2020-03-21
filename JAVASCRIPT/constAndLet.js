86a22a437e731065af7e7d07a80c988d18903e57
nitish166 <knitish123@gmail.com>
2020-03-21 20:45:31 +0530

    Updated

---
+++ JAVASCRIPT/constAndLet.js
@@ -0,0 +1,12 @@
+
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    const PI = Math.PI;
+    const r = +(readLine());
+    // Print the area of the circle:
+    const area = PI * r*r;
+    console.log(area);
+    // Print the perimeter of the circle:
+    const perimeter = 2 * PI*r;
+    console.log(perimeter);
+}
