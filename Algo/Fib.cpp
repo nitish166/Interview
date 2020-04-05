7590be5f384bcfee9338c54afb67480674c37f6a
nitish166 <knitish123@gmail.com>
2020-04-05 18:28:18 +0530

    Fibonacci Series Updated

--- Algo/Fib.cpp
+++ Algo/Fib.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+// This solution will take huge amount of time to calculate Fibonacci Series :)
+
 int Fib(int n)
 {
 	if(n==0 || n==1)
@@ -8,6 +11,25 @@ int Fib(int n)
 		return 1;
 	}
 	return Fib(n-1) + Fib(n-2);
+}
+
+
+int Fib2(int n, int* arr)
+{
+	if(n==0 || n==1)
+	{
+		return 1;
+	}
+
+	if(arr[n]>0)
+	{
+		return arr[n];
+	}
+
+	int output = Fib2(n-1, arr) + Fib2(n-2, arr);
+	arr[n] = output;
+	return output;
+
 }
 
 
