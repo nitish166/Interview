6dab43e97ebdedbafb1d46876e1a248b91c1a63a
nitish166 <knitish123@gmail.com>
2020-04-05 18:43:07 +0530

    Fibonacci Series Updated

--- Algo/DP/Fib.cpp
+++ Algo/DP/Fib.cpp
@@ -13,6 +13,7 @@ int Fib(int n)
 	return Fib(n-1) + Fib(n-2);
 }
 
+// Shall take less time compare to first solution
 
 int Fib2(int n, int* arr)
 {
@@ -30,6 +31,23 @@ int Fib2(int n, int* arr)
 	arr[n] = output;
 	return output;
 
+}
+
+
+// Iterative solution
+
+int fibi(int n)
+{
+	int* arr = new int[n+1];
+	arr[0] = 1;
+	arr[1] = 1;
+	for(int i=2; i<=n; i++)
+	{
+		arr[i] = arr[i-1] + arr[i-2];
+	}
+	int output = arr[n];
+	delete[] arr;
+	return output;
 }
 
 
