655f34108f52c3b1e80d545a1d896c852372d6b8
nitish166 <knitish123@gmail.com>
2020-04-08 12:02:49 +0530

    ChangeCoin Problem

--- Algo/DP/CoinChange.cpp
+++ Algo/DP/CoinChange.cpp
@@ -5,7 +5,7 @@ using namespace std;
 typedef long long int LL;
 
 
-int coin_change(int n, int* d, int numD)
+int coin_change(int n, int* d, int m, int** output)
 {
 	if(n == 0)
 	{
@@ -15,23 +15,63 @@ int coin_change(int n, int* d, int numD)
 	{
 		return 0;
 	}
-	if(numD == 0)
+	if(m == 0)
 	{
 		return 0;
 	}
-
-	int first = coin_change(n-d[0], d, numD);
-	int second = coin_change(n, d+1, numD-1);
-	return first+second;
+	if(output[n][m]>-1)
+	{
+		return output[n][m];
+	}
+	int first = coin_change(n-d[0], d, m, output);
+	int second = coin_change(n, d+1, m-1, output);
+	output[n][m] = first+second;
+	return output[n][m];
 }
-
 
 
 int main()
 {
  
    ios_base:: sync_with_stdio(false); cin.tie(0);
-   int d[2] = {1,2};
-   cout<<coin_change(4, d, 2)<<endl;
+   
+
+   // Test cases 
+   int t;
+   cin>>t;
+   while(t--)
+   {
+   		// number of coins
+   		int m;
+   		cin>>m;
+   		int* d = new int[m];
+   		for(int i=0;i<m;i++)
+   		{
+   			cin>>d[i];
+   		}
+
+   		// Denoting the cents
+   		int n;
+   		cin>>n;
+
+   		// allocated 2D array, (n+1)*(numD+1)
+   		int size = (n+1)*(m+1);
+   		int** output = new int*[size];
+   		for(int i=0; i<size; i++)
+   		{
+   			output[i] = new int[size];
+   			for(int j=0; j<n; j++)
+   			{
+   				output[i][j] = -1;
+   			}
+   		}
+
+   		cout<<coin_change(n, d, m, output)<<endl;
+   }
+
+
+
+
+   
    return 0;
 }
