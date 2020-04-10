20ca2e91f7c486ac1e664d8c3857d981ae38c7a7
nitish166 <knitish123@gmail.com>
2020-04-10 11:55:28 +0530

    Using Iterative Method LIS

--- Algo/DP/LongestCommonSubsequence.cpp
+++ Algo/DP/LongestCommonSubsequence.cpp
@@ -83,6 +83,54 @@ int lcs2(char* s1, char* s2)
 
 
 
+int lcsI(char* s1, char* s2)
+{
+	int m=strlen(s1);
+	int n=strlen(s2);
+
+	int** dp = new int*[m+1];
+	for(int i=0;i<=m;i++)
+	{
+		dp[i] = new int[n+1];
+	}
+
+	for(int i=0;i<=m;i++)
+	{
+		dp[i][0] =0;
+	}
+
+	for(int i=0;i<=n;i++)
+	{
+		dp[0][i] =0;
+	}
+
+	for(int i=1; i<=m; i++)
+	{
+		for(int j=1; j<=n; j++)
+		{
+			if(s1[m-i] == s2[n-j])
+			{
+				dp[i][j] = 1+dp[i-1][j-1];
+			}
+			else
+			{
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+			}
+		}
+	}
+
+	int ans = dp[m][n];
+	// delete 2d array
+	for(int i=0;i<=m;i++)
+	{
+		delete [] dp[i];
+	}
+	delete [] dp;
+	return ans;
+
+}
+
+
 
 int main()
 {
@@ -93,6 +141,7 @@ int main()
 
    cin>>a;
    cin>>b;
+   cout<<lcsI(a,b)<<endl;
    cout<<lcs2(a, b)<<endl;
    cout<<LongestCommonSubsequence(a, b)<<endl;
    return 0;
