970a10a08a773812d6bb973e052ba4a7694e46cd
nitish166 <knitish123@gmail.com>
2020-04-15 11:08:50 +0530

    Array Problem

---
+++ CP/CountingInfinity.cpp
@@ -0,0 +1,22 @@
+// Loop Concept
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	// freopen("countin.txt", "r", stdin);
+	// freopen("countout.txt", "w", stdout);
+	int n;
+	cin>>n;
+	// int i=1;
+	// while(i<=n)
+	// {
+	// 	cout<<i<<endl;
+	// 	i++;
+	// }
+	for(int i=1;i<=n;i++)
+	{
+		cout<<i<<endl;
+	}
+	return 0;
+}
