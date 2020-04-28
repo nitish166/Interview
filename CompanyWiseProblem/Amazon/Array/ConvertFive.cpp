5cfbe4698b0306b51895f7165e87d2df4d6c324f
nitish166 <knitish123@gmail.com>
2020-04-28 23:21:09 +0530

    Basic Array Problem

---
+++ CompanyWiseProblem/Amazon/Array/ConvertFive.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int convertFive(int n)
+{
+	
+}
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		cout<<convertFive(n)<<endl;
+	}
+}
