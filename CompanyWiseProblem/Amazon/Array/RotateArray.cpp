ced33bd5f1b645359be6b1148fc1ef03758d345e
nitish166 <knitish123@gmail.com>
2020-05-01 13:37:07 +0530

    Rotate Array Time: O(n)

--- CompanyWiseProblem/Amazon/Array/RotateArray.cpp
+++ CompanyWiseProblem/Amazon/Array/RotateArray.cpp
@@ -1,10 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int gcd(int a, int b)
+{
+	if(b==0)
+	{
+		return a;
+	}
+	else
+	{
+		return gcd(b, a%b);
+	}
+}
+
 
 void rotateArray(int* arr, int n, int d)
 {
-	
+	d = d%n;
+	int g_c_d = gcd(d,n);
+	for(int i=0; i<g_c_d; i++)
+	{
+		int temp = arr[i];
+		int j =i;
+		while(true)
+		{
+			int k = j+d;
+			if(k>=n)
+			{
+				k = k-n;
+			}
+			if(k==i)
+			{
+				break;
+			}
+			arr[j] = arr[k];
+			j = k;
+
+		}
+		arr[j] = temp;
+	}
 }
 
 int main()
@@ -26,6 +60,6 @@ int main()
 		{
 			cout<<arr[i]<<" ";
 		}
-		cout<<end;
+		cout<<endl;
 	}
 }
