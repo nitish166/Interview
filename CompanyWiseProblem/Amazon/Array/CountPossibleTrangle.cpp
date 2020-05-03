a7ea2910aa2b1785d77d3843e2efd790bc8ca313
nitish166 <knitish123@gmail.com>
2020-05-03 14:06:56 +0530

    Time: O(n*n)

--- CompanyWiseProblem/Amazon/Array/CountPossibleTrangle.cpp
+++ CompanyWiseProblem/Amazon/Array/CountPossibleTrangle.cpp
@@ -29,6 +29,40 @@ int findPossibleTrangle(long long size)
 	return count;
 }
 
+// Two pointer approch
+// Time Complexity: O(n*n)
+
+
+int findPossibleTrangle(long long size)
+{
+	int count=0;
+    sort(arr, arr+n);
+    
+    for(int i=n-1; i>=1; i--)
+    {
+        int l = 0;
+        int r = i-1;
+        while(l<r)
+        {
+            if(arr[l]+arr[r]>arr[i])
+            {
+                count += r-l;
+                r--;
+            }
+            else
+            {
+                l++;
+            }
+        }
+    }
+    return count;
+}
+
+
+
+
+
+
 int main()
 {
 	long long t;
