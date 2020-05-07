083f8c6272f88775757dc57b659b903931222cc9
nitish166 <knitish123@gmail.com>
2020-05-07 19:37:26 +0530

    Accepted, In One Go!

---
+++ CompanyWiseProblem/Amazon/Array/ProductArray.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void ProductArray(int* arr, int n)
+{
+	vector<int> ans;
+	
+	int prod;
+	for(int i=0; i<n; i++)
+	{
+		prod=1;
+		for(int j=0; j<n; j++)
+		{
+			if(i!=j)
+			{
+				prod *=arr[j]; 
+				
+			}
+		}
+		ans.push_back(prod);
+
+	}
+
+
+	for(int i=0; i<ans.size(); i++)
+	{
+		cout<<ans[i]<<" ";
+	}
+}
+
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int arr[n];
+		for(int i=0; i<n; i++)
+		{
+			cin>>arr[i];
+		}
+
+		ProductArray(arr, n);
+		cout<<endl;
+	}
+	return 0;
+}
