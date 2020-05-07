066d06e28050fd57ad9222effc2d4ce5218d86ab
nitish166 <knitish123@gmail.com>
2020-05-07 20:32:05 +0530

    Accepted, Happy Now *_*

--- CompanyWiseProblem/Amazon/Array/BinaryArraySorting.cpp
+++ CompanyWiseProblem/Amazon/Array/BinaryArraySorting.cpp
@@ -18,6 +18,39 @@ vector<int> sortBinaryArray(vector<int> binArray)
 }
 
 
+// Time Complexity: O(n)
+
+vector<int> SortBinaryArray(vector<int> binArray)
+{
+    // Your code goes here 
+    int zero=0, one=0;
+    for(int i=0; i<binArray.size(); i++)
+    {
+        if(binArray[i]==0)
+        {
+            zero++;
+        }
+        else
+        {
+            one++;
+        }
+    }
+    vector<int> ans;
+    for(int i=0; i<zero; i++)
+    {
+        ans.push_back(0);
+    }
+    
+    for(int i=0; i<one; i++)
+    {
+        ans.push_back(1);
+    }
+    
+    return ans;
+}
+
+
+
 int main()
 {
 	int t;
