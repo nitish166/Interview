5ae3c7dc3293e8f969fed6beae95d7f7dbbc9577
nitish166 <knitish123@gmail.com>
2020-05-08 21:08:44 +0530

    Space complexithy: O(n) || Need to improve in O(1)

---
+++ CompanyWiseProblem/Amazon/Array/RearrangeArray.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// BruteForce Solution
+// using Extra Space
+// Space Complexity: O(n)
+// Time Complexity: O(n)
+
+void rearrangeArray(int* arr, int n)
+{
+	vector<int> ans;
+	for(int i=0; i<n; i++)
+	{
+		int temp = arr[i];
+		int x = arr[temp];
+		temp = x;
+		ans.push_back(temp);
+	}
+
+	for(int i=0; i<n; i++)
+	{
+		cout<<ans[i]<<" ";
+	}
+}
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int arr[n];
+		for(int i=0; i<n; i++)
+		{
+			cin>>arr[i];
+		}
+
+		rearrangeArray(arr, n);
+		cout<<endl;
+	}
+}
