652d578da12705c9f32a32c3e98d72930b8be45a
nitish166 <knitish123@gmail.com>
2020-05-09 13:35:40 +0530

    Saw Solution || Time Complexity: O(n)

--- CompanyWiseProblem/Amazon/Array/maxIndexDifference.cpp
+++ CompanyWiseProblem/Amazon/Array/maxIndexDifference.cpp
@@ -6,22 +6,64 @@ using namespace std;
 // Time Complexity: O(n)
 // Space Complexity: O(1)
 
+// int maxDiff(int* arr, int n)
+// {
+// 	int dis=0;
+// 	for(int i=0; i<n; i++)
+// 	{
+// 		for(int j=i+1; j<n; j++)
+// 		{
+// 			if(arr[i]<=arr[j])
+// 			{
+// 				int temp = j-i;
+// 				dis = max(temp, dis);
+// 			}
+// 		}
+// 	}
+
+// 	return dis;
+// }
+
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+
 int maxDiff(int* arr, int n)
 {
-	int dis=0;
-	for(int i=0; i<n; i++)
+	int* larray = new int[(sizeof(int)*n)];
+	int* rarray = new int[(sizeof(int)*n)];
+
+	larray[0] = arr[0];
+	for(int i=1; i<n; i++)
 	{
-		for(int j=i+1; j<n; j++)
+		larray[i] = min(arr[i], larray[i-1]);
+	}
+
+	rarray[n-1] = arr[n-1];
+
+	for(int  j = n-2; j>=0; j--)
+	{
+		rarray[j] = max(arr[j], rarray[j+1]);
+	}
+
+
+	int i=0, j=0, diff=-1;
+
+	while(j<n && i<n)
+	{
+		if(larray[i]<= rarray[j])
 		{
-			if(arr[i]<=arr[j])
-			{
-				int temp = j-i;
-				dis = max(temp, dis);
-			}
+			diff = max(diff, j-i);
+			j++;
+		}
+		else
+		{
+			i++;
 		}
 	}
 
-	return dis;
+	return diff;
 }
 
 
