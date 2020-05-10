e19dd572bee37fc7f075679ee34aee98ad11657d
nitish166 <knitish123@gmail.com>
2020-05-10 21:43:48 +0530

    Segementation fault for base case || n==2 || Need to check || issue

--- CompanyWiseProblem/Amazon/Array/celebrityProblem.cpp
+++ CompanyWiseProblem/Amazon/Array/celebrityProblem.cpp
@@ -4,38 +4,117 @@ using namespace std;
 // Brute Force Solution
 // Time Complexity: O(n^2)
 
+// int know(int a, int b, int** mat)
+// {
+// 	return mat[a][b];
+// }
+
+
+// int getId(int** mat, int n)
+// {
+// 	int indegree[n] ={0}, outdegree[n] ={0};
+
+// 	for(int i=0; i<n; i++)
+// 	{
+// 		for(int j=0; j<n; j++)
+// 		{
+// 			int x = know(i, j, mat);
+
+// 			outdegree[i] +=x;
+// 			indegree[j] +=x;
+// 		}
+// 	}
+
+// 	for(int i=0; i<n; i++)
+// 	{
+// 		if(outdegree[i]==0 && indegree[i]==n-1)
+// 			return i;
+// 	}
+
+// 	return -1;
+// }
+
+
+// using stack method
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+
 int know(int a, int b, int** mat)
 {
 	return mat[a][b];
 }
 
-
 int getId(int** mat, int n)
 {
-	int indegree[n] ={0}, outdegree[n] ={0};
+	// This about this test case?
+	if(n==2)
+	{
+		return -1;
+	}
+	stack<int> s;
 
+	int C; // celebrity
+
+	// push everybody to stack
 	for(int i=0; i<n; i++)
 	{
-		for(int j=0; j<n; j++)
-		{
-			int x = know(i, j, mat);
+		s.push(i);
+	}
 
-			outdegree[i] +=x;
-			indegree[j] +=x;
+	// Extract top 2 from stack
+
+	int A = s.top();
+	s.pop();
+
+	int B = s.top();
+	s.pop();
+
+	// find the potential celebrity
+
+	while(s.size()>1)
+	{
+		if(know(A, B, mat))
+		{
+			A = s.top();
+			s.pop();
+		}
+		else
+		{
+			B = s.top();
+			s.pop();
 		}
 	}
 
-	for(int i=0; i<n; i++)
+	// potential candidate?
+
+	C = s.top();
+	s.pop();
+
+	// Check last candidate
+
+	if(know(C, B, mat))
 	{
-		if(outdegree[i]==0 && indegree[i]==n-1)
-			return i;
+		C=B;
 	}
 
-	return -1;
+	if(know(C, A, mat))
+	{
+		C=A;
+	}
+
+	// Check C is actually a celebrity or not
+
+	for(int i=0; i<n; i++)
+	{
+		if((i != C) && (know(C, i, mat) || !know(i, C, mat)))
+		{
+			return -1;
+		}
+	}
+
+	return C;
 }
-
-
-
 
 
 
