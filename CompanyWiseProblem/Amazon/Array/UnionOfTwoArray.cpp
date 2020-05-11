3414a703478c6e8c3ca3deabed5cacdfe2f9c18a
nitish166 <knitish123@gmail.com>
2020-05-11 12:15:14 +0530

    Time complexity: O(n+m) || Not handle for duplicate cases

--- CompanyWiseProblem/Amazon/Array/UnionOfTwoArray.cpp
+++ CompanyWiseProblem/Amazon/Array/UnionOfTwoArray.cpp
@@ -1,36 +1,78 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Time Complexity: O(nlogn)
+// Space complexity: O(n)
+
+// void unionOfTwoArray(int* arr1, int* arr2, int n, int m)
+// {
+// 	set<int> s1;
+// 	for(int i=0; i<n; i++)
+// 	{
+// 		s1.insert(arr1[i]);
+// 	}
+
+// 	for(int i=0; i<m; i++)
+// 	{
+// 		s1.insert(arr2[i]);
+// 	}
+
+
+// 	set<int>:: iterator it;
+// 	vector<int> ans;
+
+// 	for(it = s1.begin(); it!=s1.end(); it++)
+// 	{
+// 		ans.push_back(*it);
+// 	}
+
+// 	sort(ans.begin(), ans.end());
+
+// 	for(int i=0; i<ans.size(); i++)
+// 	{
+// 		cout<<ans[i]<<" ";
+// 	}
+
+// }
+
+// Time Complexity: O(n+m)
+// Not handle duplicate elements
+
 void unionOfTwoArray(int* arr1, int* arr2, int n, int m)
 {
-	set<int> s1;
-	for(int i=0; i<n; i++)
+	int i=0, j=0;
+	while(i<n && j<m)
 	{
-		s1.insert(arr1[i]);
+		if(arr1[i]<arr2[j])
+		{
+			cout<<arr1[i++]<<" ";
+		}
+		else if(arr2[j]<arr1[i])
+		{
+			cout<<arr2[j++]<<" ";
+		}
+		// both elements are equal in array
+		else
+		{
+			cout<<arr1[i++]<<" ";
+			j++;
+		}
 	}
 
-	for(int i=0; i<m; i++)
+	// For remaing larger element
+	while(i<n)
 	{
-		s1.insert(arr2[i]);
+		cout<<arr1[i++]<<" ";
 	}
 
-
-	set<int>:: iterator it;
-	vector<int> ans;
-
-	for(it = s1.begin(); it!=s1.end(); it++)
+	while(j<m)
 	{
-		ans.push_back(*it);
+		cout<<arr2[j++]<<" ";
 	}
-
-	sort(ans.begin(), ans.end());
-
-	for(int i=0; i<ans.size(); i++)
-	{
-		cout<<ans[i]<<" ";
-	}
-
 }
+
+
+
 
 int main()
 {
