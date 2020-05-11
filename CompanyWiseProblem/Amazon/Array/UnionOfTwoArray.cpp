9b60db1a146be1e3cdbf765efd6d804b4a25e3d2
nitish166 <knitish123@gmail.com>
2020-05-11 14:14:11 +0530

    Accepted || Saw Solution for handling duplicate elements!

--- CompanyWiseProblem/Amazon/Array/UnionOfTwoArray.cpp
+++ CompanyWiseProblem/Amazon/Array/UnionOfTwoArray.cpp
@@ -43,6 +43,19 @@ void unionOfTwoArray(int* arr1, int* arr2, int n, int m)
 	int i=0, j=0;
 	while(i<n && j<m)
 	{
+		// avoiding identical element in arr1
+		while(i+1<n && arr1[i]==arr1[i+1])
+		{
+			i++;
+		}
+
+		// avoiding identical element in arr2
+		while(j+1<m && arr2[j]==arr2[j+1])
+		{
+			j++;
+		}
+
+		// printing to smaller number to maintain ascending order
 		if(arr1[i]<arr2[j])
 		{
 			cout<<arr1[i++]<<" ";
@@ -51,22 +64,34 @@ void unionOfTwoArray(int* arr1, int* arr2, int n, int m)
 		{
 			cout<<arr2[j++]<<" ";
 		}
-		// both elements are equal in array
+		// if arr[i] is same as arr2[j], number is printed once and i and j are incremented
 		else
 		{
 			cout<<arr1[i++]<<" ";
 			j++;
 		}
 	}
+	// loops break when either i or j reach end of arr1 and arr2 respectively
 
-	// For remaing larger element
 	while(i<n)
+		// printing elements of arr1 if any left
 	{
+		// avoiding repetition
+		while(i+1<n && arr1[i]==arr1[i+1])
+		{
+			i++;
+		}
 		cout<<arr1[i++]<<" ";
 	}
 
 	while(j<m)
+		// printing elements of arr2 if any left
 	{
+		// Avoiding repetition
+		while(j+1<m && arr2[j]==arr2[j+1])
+		{
+			j++;
+		}
 		cout<<arr2[j++]<<" ";
 	}
 }
