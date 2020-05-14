f87fe6768701dc3fedff9a31c604c7819e904718
nitish166 <knitish123@gmail.com>
2020-05-14 23:58:07 +0530

    Accepted | Time complexity: O(n)

---
+++ CompanyWiseProblem/Amazon/Searching/TransationPoint.cpp
@@ -0,0 +1,16 @@
+int transitionPoint(int arr[], int n) {
+    // code here
+    bool temp = false;
+    for(int i=0; i<n; i++)
+    {
+        if(arr[i]==1)
+        {
+            temp = true;
+            return i;
+        }
+    }
+    if(temp==false)
+    {
+        return -1;
+    }
+}
