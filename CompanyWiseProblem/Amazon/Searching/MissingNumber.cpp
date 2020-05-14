bce2c9a6b869b37a2c44d14c5b77bfbaafe01aeb
nitish166 <knitish123@gmail.com>
2020-05-14 23:51:51 +0530

    Time Complexity: O(n)

--- CompanyWiseProblem/Amazon/Searching/MissingNumber.cpp
+++ CompanyWiseProblem/Amazon/Searching/MissingNumber.cpp
@@ -10,3 +10,20 @@ int MissingNumber(vector<int>& array, int n) {
     
     return sum-temp;
 }
+
+
+// 2nd method
+// Time complexity: O(n)
+// space Complexity: O(1)
+
+int MissingNumber(vector<int>& array, int n) {
+    int xor_array = 0, xor_n = 0;
+
+    // xor of all array elements
+    for (int i = 0; i < n - 1; i++) xor_array = xor_array ^ array[i];
+
+    // xor of n natural numbers
+    for (int i = 1; i <= n; i++) xor_n = xor_n ^ i;
+
+    return xor_n ^ xor_array;
+}
