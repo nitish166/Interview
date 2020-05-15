fda95ac3c05f064554e911dfeda3f8574eb25541
nitish166 <knitish123@gmail.com>
2020-05-15 15:45:27 +0530

    Time complexity: O(logn) | Efficent solution

--- CompanyWiseProblem/Amazon/Searching/indexofAnExtraElement.cpp
+++ CompanyWiseProblem/Amazon/Searching/indexofAnExtraElement.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+// Linear search method
 // Time complexity: O(n)
 // Space complexity: O(1)
 
@@ -14,6 +14,42 @@ int findExtra(int* a, int* b, int n)
 			return i;
 		}
 	}
+}
+
+
+// Binary Search Method
+
+int findExtra(int* a, int* b, int n)
+{
+	int index = n;
+
+	int l = 0, r = n-1;
+	while(l<=r)
+	{
+		int mid = (l+r)/2;
+
+		// if middle element is same
+		// of both arrays, it means
+		// that extra element is after mid
+		// so we update left to mid+1
+
+		if(a[mid]==b[mid])
+		{
+			l = mid+1;
+		}
+		// if middle elements is different 
+		// of the arrays, it means that 
+		// the index we are searching for 
+		// is either mid, or before mid
+		// hence we update right to mid-1
+		else
+		{
+			index = mid;
+			right = mid-1;
+		}
+	}
+
+	return index;
 }
 
 
