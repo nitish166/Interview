1b2c79cdc4a3e70544aa7dd5f7a58a0fa6bd2f67
nitish166 <knitish123@gmail.com>
2020-06-23 12:04:12 +0530

    Middle of the linked list

---
+++ LeetCodeApril/Day-8.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+// Approch : Hare and tortoise
+// Time Complexity: O(n)
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        
+        ListNode *hare, *tortoise;
+        hare = tortoise = head;
+        
+        while(hare && hare->next)
+        {
+            hare = hare->next->next;
+            tortoise = tortoise->next;
+        }
+        
+        return tortoise;
+    }
+};
