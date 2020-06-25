10cf553e55a613a015ca74ce5297e6e689ab2025
nitish166 <knitish123@gmail.com>
2020-06-25 13:54:27 +0530

    Day-25

--- LeetCodeJune/DuplicateNumber.cpp
+++ LeetCodeJune/DuplicateNumber.cpp
@@ -16,3 +16,20 @@ public:
         return ans;
     }
 };
+
+
+//Another Method
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+        int ans =0;
+        for(int i=0; i<nums.size()-1; i++)
+        {
+            if(nums[i]==nums[i+1]) ans = nums[i];
+        }
+        return ans;
+    }
+};
