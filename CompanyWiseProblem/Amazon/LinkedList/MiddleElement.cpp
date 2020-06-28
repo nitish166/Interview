c0c259698ac9687187c33cfab38f7e615d30abec
nitish166 <knitish123@gmail.com>
2020-06-28 23:58:24 +0530

    Accepted

---
+++ CompanyWiseProblem/Amazon/LinkedList/MiddleElement.cpp
@@ -0,0 +1,20 @@
+int getMiddle(Node *head)
+{
+   Node* temp=head;
+    Node* mid=head;
+   
+    if(head==NULL)
+        return -1;
+    
+   while( mid != NULL  &&  mid->next != NULL )
+    {  
+        mid=mid->next->next;
+        // this pointer moves 2 nodes ahead everytime loop is run
+        
+        temp=temp->next;
+        // this pointer moves 1 node ahead everytime loop is run
+    }
+    
+    // since temp was moving with half speed, it is there at halfway point
+    return temp->data;
+}
