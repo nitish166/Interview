23fe0360cd917c25f0397cb2181d84abbbfaf915
nitish166 <knitish123@gmail.com>
2020-06-29 22:59:51 +0530

    Day-29

--- CompanyWiseProblem/Amazon/LinkedList/MiddleElement.cpp
+++ CompanyWiseProblem/Amazon/LinkedList/MiddleElement.cpp
@@ -1,12 +1,12 @@
 int getMiddle(Node *head)
 {
-   Node* temp=head;
+    Node* temp=head;
     Node* mid=head;
    
     if(head==NULL)
         return -1;
     
-   while( mid != NULL  &&  mid->next != NULL )
+    while( mid != NULL  &&  mid->next != NULL )
     {  
         mid=mid->next->next;
         // this pointer moves 2 nodes ahead everytime loop is run
