4a380109bf1406d53af9210d21087122b3ced80a
nitish166 <knitish123@gmail.com>
2020-07-03 23:47:51 +0530

    Day-1 updates

--- LeetCode/Easy/MajorityElement.cpp
+++ LeetCode/Easy/MajorityElement.cpp
@@ -34,3 +34,20 @@ public:
         return nums[nums.size()/2];
     }
 };
+
+
+// 3rd Method
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int counter = 0, majority;
+        for (int num : nums) {
+            if (!counter) {
+                majority = num;
+            }
+            counter += num == majority ? 1 : -1;
+        }
+        return majority;
+    }
+};
