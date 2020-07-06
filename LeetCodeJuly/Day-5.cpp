6ba666b06a552a6c624f769da8e441bee0ef5049
nitish166 <knitish123@gmail.com>
2020-07-06 19:18:45 +0530

    Hamming Distance

---
+++ LeetCodeJuly/Day-5.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        
+        int z = x^y;
+        int count =0;
+        while(z>0)
+        {
+           count +=z&1;
+           z >>=1;
+        }
+        return count;
+    }
+};
