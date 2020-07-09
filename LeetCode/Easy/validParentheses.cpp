740e1d0403fa15871f7131f62251396ae667aa68
nitish166 <knitish123@gmail.com>
2020-07-09 20:10:48 +0530

    Leetcode | Basic

---
+++ LeetCode/Easy/validParentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+         while(s.contains("()") || s.contains("[]") || s.contains("{}"))
+        {
+            s = s.replace("()", "");
+            s = s.replace("[]", "");
+            s = s.replace("{}", "");
+        }
+        
+        if(s.length()==0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
