49d7bab7ac557515b3bde3876f66a8aca45cd1bd
nitish166 <knitish123@gmail.com>
2020-07-23 23:57:46 +0530

    Tree |InorderTraversal

---
+++ CompanyWiseProblem/Amazon/Tree/InorderTraversal.cpp
@@ -0,0 +1,20 @@
+
+void inOrderFun(vector<int> &res, Node* root)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    inOrderFun(res, root->left);
+    res.push_back(root->data);
+    inOrderFun(res, root->right);
+}
+
+vector<int> inOrder(Node* root)
+{
+  // Your code here
+  vector<int> res;
+  inOrderFun(res, root);
+  return res;
+  
+}
