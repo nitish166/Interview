efa4252654eeb37f0e2a340004b5addd20842c16
nitish166 <knitish123@gmail.com>
2020-08-17 00:53:31 +0530

    RightSideOfBinaryTree | Recursive | Tree

--- CompanyWiseProblem/Amazon/Tree/BinaryTreeRightSideView.cpp
+++ CompanyWiseProblem/Amazon/Tree/BinaryTreeRightSideView.cpp
@@ -49,3 +49,31 @@ public:
         return res;
     }
 };
+
+
+
+// recursive solution
+class Solution {
+public:
+    
+    void rightSide(TreeNode* root, int level, vector<int>&res)
+    {
+        if(root==NULL)
+        {
+            return;
+        }
+        if(res.size()<level)
+        {
+            res.push_back(root->val);
+        }
+        rightSide(root->right, level+1, res);
+        rightSide(root->left, level+1, res);
+    }
+    
+    vector<int> rightSideView(TreeNode* root) {
+        
+        vector<int> res;
+        rightSide(root, 1, res);
+        return res;
+    }
+};
