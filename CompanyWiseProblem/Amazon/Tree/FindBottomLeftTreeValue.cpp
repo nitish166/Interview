36ddfa811dae611708477dca6dbb39d9f8ba8a86
nitish166 <knitish123@gmail.com>
2020-08-17 01:09:30 +0530

    BottomLeftTreeValue | Tree

---
+++ CompanyWiseProblem/Amazon/Tree/FindBottomLeftTreeValue.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        
+        vector<int> res;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int n = q.size();
+            for(int i=0; i<n; i++)
+            {
+                TreeNode* temp = q.front();
+                q.pop();
+                if(i==0)
+                {
+                    res.push_back(temp->val);
+                }
+                if(temp->left!=NULL)
+                {
+                    q.push(temp->left);
+                }
+                if(temp->right!=NULL)
+                {
+                    q.push(temp->right);
+                }
+            }
+        }
+        int ans = res[res.size()-1];
+        return ans;
+    }
+};
