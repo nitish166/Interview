c657d9a0755ed8e0d008b7a568704e68f8b49989
nitish166 <knitish123@gmail.com>
2020-08-26 21:12:59 +0530

    Burn Tree

---
+++ InterViewBit/Tree/BurnTree.cpp
@@ -0,0 +1,11 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int Solution::solve(TreeNode* A, int B) {
+}
