ac88410680ee01cfe4603d3056f3d92da79abb1f
nitish166 <knitish123@gmail.com>
2020-09-03 02:35:50 +0530

    Mirror | Amazon | Tree

---
+++ CompanyWiseProblem/Amazon/Tree/mirror.cpp
@@ -0,0 +1,14 @@
+void mirror(Node* node) 
+{
+     // Your Code Here
+     if(node==NULL)
+     {
+         return;
+     }
+     mirror(node->left);
+     mirror(node->right);
+     Node* temp = node->left;
+     node->left  = node->right;
+     node->right = temp;
+     
+}
