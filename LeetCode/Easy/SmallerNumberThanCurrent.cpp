2723654479db2072161c230eef766ecd68187da7
nitish166 <knitish123@gmail.com>
2020-09-27 12:36:54 +0530

    Improvement | O(n)

--- LeetCode/Easy/SmallerNumberThanCurrent.cpp
+++ LeetCode/Easy/SmallerNumberThanCurrent.cpp
@@ -20,3 +20,29 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        
+        vector<int> ans(nums.size());
+        vector<int> a(101);
+        vector<int> b(101);
+        // frequency array
+        for(int i=0; i<nums.size(); i++)
+        {
+            a[nums[i]]++;
+        }
+        // prefix sum array
+        for(int i=1; i<101; i++)
+        {
+            b[i] = b[i-1]+a[i-1];
+        }
+        // final ans
+        for(int i=0; i<nums.size(); i++)
+        {
+            ans[i] = b[nums[i]];
+        }
+        return ans;
+    }
+};
