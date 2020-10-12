5d16c85678c2616b22c3da5193ee91b746c63b39
nitish166 <knitish123@gmail.com>
2020-10-12 22:43:07 +0530

    First Set bit

---
+++ CompanyWiseProblem/Amazon/BIT Magic/firstSetBit.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int ans = ffs(n);
+	    cout<<ans<<endl;
+	}
+	return 0;
+}
