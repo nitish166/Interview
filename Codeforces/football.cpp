696d32485643362e6ff52ae8dda60dc1df934bb1
nitish166 <knitish123@gmail.com>
2020-10-30 23:42:17 +0530

    Bug

--- Codeforces/football.cpp
+++ Codeforces/football.cpp
@@ -1,22 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int calculatePair()
-{
-
-}
-
-
 int main()
 {
 	int n;
 	cin>>n;
-	vector<string>;
+	vector<string> v;
 	for(int i=0; i<n; i++)
 	{
 		string str;
 		cin>>str;
-
+		v.push_back(str);
 	}
+	map<string, int> m;
+	for(int i=0; i<n; i++)
+	{
+		m[v[i]]++;
+	}
+	int max = INT_MIN;
+	map<string, int> :: iterator it;
+	for(it= m.begin(); it !=m.end(); it++)
+	{
+		if(it->second > max)
+		{
+			max = it->second;
+		}
+	}
+
+	for(it= m.begin(); it !=m.end(); it++)
+	{
+		if(it->second = max)
+		{
+			cout<<it->first<<endl;
+			break;
+		}
+	}
+
 	return 0;
 }
